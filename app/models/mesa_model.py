7bfc48b5b22c499c9bd11ba65c5941e0f3fe938e
Silvio Romano Neto <silvioromanoneto@gmail.com>
2021-07-09 17:27:20 -0300

    fix: mesa model

--- app/models/mesa_model.py
+++ app/models/mesa_model.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
 from sqlalchemy import Column, Integer
+from sqlalchemy.orm import relationship, backref
+
 
 from app.configs.database import db
 
+@dataclass
 class MesaModel(db.Model):
     id: int
     numero: int
@@ -11,3 +14,5 @@ class MesaModel(db.Model):
 
     id = Column(Integer, primary_key=True)
     numero = Column(Integer, nullable=False)
+
+    lista_contas = relationship("ContaModel", backref=backref("mesa", uselist=False))
