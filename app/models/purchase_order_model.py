55e6ef5502c204b2889c4cc9e40f968d723ebd9d
vinicius <vinicius.campello@hotmail.com>
2021-07-15 18:12:07 -0300

    feat: improve models and tests_v view

--- app/models/purchase_order_model.py
+++ app/models/purchase_order_model.py
@@ -9,18 +9,18 @@ from sqlalchemy.orm import backref, relationship
 @dataclass
 class PurchaseOrderModel(db.Model):
     id: int
-    id_fornecedor: int
-    data: date
-    lista_produtos: list
+    id_provider: int
+    date: date
 
     __tablename__ = 'purchase_order'
 
     id = Column(Integer, primary_key=True)
 
-    id_fornecedor = Column(Integer, ForeignKey(
-        "provider.id"), nullable=False)
-    data = Column(DateTime, nullable=False)
+    id_provider = Column(Integer, ForeignKey("provider.id"), nullable=False)
+    date = Column(DateTime, nullable=False)
 
-    lista_produtos = relationship(
-        'ProdutoModel', backref=backref('purchase_order_list'), secondary='produto_purchase_order'
+    products_list = relationship(
+        'ProductModel',
+        backref=backref('purchase_order_list'),
+        secondary='produto_purchase_order',
     )
--- app/views/tests_v_view.py
+++ app/views/tests_v_view.py
@@ -18,20 +18,26 @@ bp = Blueprint('bp_tests_v', __name__, url_prefix='/api')
 @bp.route("/", methods=["GET"])
 def get():
     session = db.session
-    gerentes_list = session.query(ManagerModel).join(UserModel).all()
-    garcons_list = session.query(WaiterModel).join(UserModel).all()
-    operadores_list = session.query(OperatorModel).join(UserModel).all()
-    conta_list = (
-        session.query(ProductModel).join(AccountProductModel).join(AccountModel).all()
+    gerentes_list = (
+        session.query(ManagerModel)
+        .join(UserModel, UserModel.id == ManagerModel.id_user)
+        .all()
     )
+    # garcons_list = session.query(WaiterModel).join(UserModel).all()
+    # operadores_list = session.query(OperatorModel).join(UserModel).all()
+    # conta_list = (
+    #     session.query(ProductModel).join(AccountProductModel).join(AccountModel).all()
+    # )
+
+    # ROTA DE VENDAS
 
     data1 = [info.usuario.username for info in gerentes_list]
 
-    data2 = [info.usuario.username for info in garcons_list]
+    # data2 = [info.usuario.username for info in garcons_list]
 
-    data3 = [info.usuario.username for info in operadores_list]
+    # data3 = [info.usuario.username for info in operadores_list]
 
-    data4 = [c.garcom_da_conta_model for info in conta_list for c in info.contas_list]
+    # data4 = [c.garcom_da_conta_model for info in conta_list for c in info.contas_list]
 
     import ipdb
 
