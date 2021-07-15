b4b816e36301a6438762451df24cc8eb15cc01fb
vinicius <vinicius.campello@hotmail.com>
2021-07-15 19:38:17 -0300

    feat: purchase ord. model translate names

--- app/models/operator_model.py
+++ app/models/operator_model.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column, String, Integer, ForeignKey
-from sqlalchemy.orm import relationship, backref
-from app.configs.database import db
 from dataclasses import dataclass
+
+from app.configs.database import db
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import backref, relationship
 
 
 @dataclass
@@ -16,8 +17,8 @@ class OperatorModel(db.Model):
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
     cpf = Column(String(11), nullable=False, unique=True)
-    id_user = Column(Integer, ForeignKey("user.id"),
-                     nullable=False, unique=True)
+    id_user = Column(Integer, ForeignKey("user.id"), nullable=False, unique=True)
 
-    cashier_list = relationship("CashierModel", backref=backref(
-        "operators_list"), secondary="operator_cashier")
+    cashier_list = relationship(
+        "CashierModel", backref=backref("operators_list"), secondary="operator_cashier"
+    )
