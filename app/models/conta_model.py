993a9074aa1cd17ad9a785aa59fcf3b37b5f72f7
vinicius <vinicius.campello@hotmail.com>
2021-07-15 12:59:18 -0300

    feat: improve testes view

--- app/models/conta_model.py
+++ app/models/conta_model.py
@@ -1,14 +1,14 @@
-from sqlalchemy import Column, Date, Integer, and_
-from sqlalchemy.sql.schema import ForeignKey
-from sqlalchemy.orm import relationship, backref
 from dataclasses import dataclass
 from datetime import date
 
 import ipdb
+from app.configs.database import db
+from sqlalchemy import Column, Date, Integer, and_
+from sqlalchemy.orm import backref, relationship
+from sqlalchemy.sql.schema import ForeignKey
 
 from .conta_produto import ContaProdutoModel
 
-from app.configs.database import db
 
 @dataclass
 class ContaModel(db.Model):
@@ -27,16 +27,28 @@ class ContaModel(db.Model):
     id_caixa = Column(Integer, ForeignKey("caixas.id"), nullable=False)
     id_garcom = Column(Integer, ForeignKey("garcons.id"), nullable=False)
     id_mesa = Column(Integer, ForeignKey("mesas.id"), nullable=False)
-    id_forma_pagamento = Column(Integer, ForeignKey("forma_pagamento.id"), nullable=False)
+    id_forma_pagamento = Column(
+        Integer, ForeignKey("forma_pagamento.id"), nullable=False
+    )
 
-    lista_produtos = relationship("ProdutoModel", secondary="conta_produto", backref=backref("contas_list"))
-    
+    lista_produtos = relationship(
+        "ProdutoModel", secondary="conta_produto", backref=backref("contas_list")
+    )
+
+    garcom_da_conta_model = relationship(
+        'GarcomModel', backref=backref('contas_virtual', uselist=False)
+    )
+
     def update_value(self):
         bill_value = 0
 
         for produto in self.lista_produtos:
-            conta_produto: ContaProdutoModel = ContaProdutoModel.query.filter(and_(ContaProdutoModel.id_produto == produto.id, ContaProdutoModel.id_conta == self.id)).first()
+            conta_produto: ContaProdutoModel = ContaProdutoModel.query.filter(
+                and_(
+                    ContaProdutoModel.id_produto == produto.id,
+                    ContaProdutoModel.id_conta == self.id,
+                )
+            ).first()
             bill_value = bill_value + (produto.preco * conta_produto.quantity)
-        
-        return bill_value        
 
+        return bill_value
--- app/views/testes_view.py
+++ app/views/testes_view.py
@@ -1,11 +1,16 @@
 from http import HTTPStatus
 
 from app.configs.database import db
-from app.models import GarcomModel, GerenteModel, OperadorModel, UserModel
-from flask import Blueprint, jsonify, request
-
-from ..custom_errors import MissingKeyError, NotFoundError, RequiredKeyError
-from ..services import UserServices
+from app.models import (
+    ContaModel,
+    ContaProdutoModel,
+    GarcomModel,
+    GerenteModel,
+    OperadorModel,
+    ProdutoModel,
+    UserModel,
+)
+from flask import Blueprint
 
 bp = Blueprint('bp_testes', __name__, url_prefix='/api')
 
@@ -16,12 +21,17 @@ def get():
     gerentes_list = session.query(GerenteModel).join(UserModel).all()
     garcons_list = session.query(GarcomModel).join(UserModel).all()
     operadores_list = session.query(OperadorModel).join(UserModel).all()
+    conta_list = (
+        session.query(ProdutoModel).join(ContaProdutoModel).join(ContaModel).all()
+    )
 
     data1 = [info.usuario.username for info in gerentes_list]
 
     data2 = [info.usuario.username for info in garcons_list]
 
     data3 = [info.usuario.username for info in operadores_list]
+
+    data4 = [c.garcom_da_conta_model for info in conta_list for c in info.contas_list]
 
     import ipdb
 
